0b2ca6e0ce3f092616a68c7ee7d8858a60c85596
CarmenoI <crmninncnt@gmail.com>
2018-10-24 10:44:24 -0400

    Project is completed adding more features in the near future

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 // var pictures = [CP.jpg, D2.jpg, DS.jpg, GT.jpeg, JF.jpg, m1.gif, S2.jpg]
-var myImage = documentgetElementByClass('p1');
-var imageArray =  [CP.jpg, D2.jpg, DS.jpg, GT.jpeg, JF.jpg, m1.gif, S2.jpg];
-var imageIndex = 1;
+//create variable for img to be called on by index value
+let myImage = document.getElementsByTagName('img')[0];
+//slideId  variable created to hold all images
+let slideId = ["image/CP.jpg", "image/D2.jpg", "image/DS.jpg", "image/GT.jpeg", "image/JF.jpg", "image/mn.gif", "image/S2.jpg"]
+let index = 0
+document.getElementById('next').onclick = nextImage
+document.getElementById('back').onclick = backImage
 
-function changeImage(){
-  myImage.setAttribute("src", imageArray[image.index]);
-  imageIndex++;
-  If (imageIndex  >  3 ) {imageIndex =0;}
+function nextImage(){
+  index += 1
+  if (slideId.length==index){
+    index = 0;
+  }
+  console.log(slideId[index])
+  myImage.src = slideId[index]
+}
+
+function backImage(){
+  index -= 1
+  if(0 > index){
+    index = slideId.length - 1;
+  }
+  console.log(slideId[index])
+  myImage.src = slideId[index]
 }
